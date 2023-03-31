bc3b61364bd61fd7d4579f4998430814855cd464
aevans <aevans@starbucks.com>
2023-03-30 22:33:36 -0700

    Trying out base requirements for npm package

---
+++ index.js
@@ -0,0 +1,10 @@
+// CSS
+// import { color } from "./styles/color.css";
+// color.red
+// export const css = 'hello';
+
+// console.log('hello css : ', css);
+
+export const printMsg = function() {
+  console.log("This is a message from the demo package");
+}
